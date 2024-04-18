4fc9ca9a8ba25691334c5f8439a719c1c27d4343
BetmanSmall <betmansmall@mail.ru>
2024-04-18 07:08:09 +0300

    init Practice9

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace CsharpSB_WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClick_ToTextSplit(object sender, RoutedEventArgs e)
+        {
+            ListBoxToSplit.ItemsSource = TextBoxToSplit.Text.Split(' ').ToList();
+            ListBoxToSplit.Items.Refresh();
+        }
+
+        private void OnButtonClick_ToTextRevers(object sender, RoutedEventArgs e)
+        {
+            TextBlockToRevers.Text = string.Join(" ", TextBoxToRevers.Text.Split(' ').Reverse());
+        }
+    }
+}
